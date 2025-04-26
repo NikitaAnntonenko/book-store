3ec3585e33de1e55c611b515a09b023656018391
nikitaantonenko <nikita.anntonenko@gmail.com>
2025-04-26 23:21:03 +0200

    Fix UI login

--- bookstore-ui/src/services/authService.js
+++ bookstore-ui/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { jwtDecode } from "jwt-decode";
 
-const API_URL = 'http://localhost:8081/api/auth/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8081/api/auth/';
 
 export const register = async (username, email, password) => {
     const response = await axios.post(API_URL + 'register', {
