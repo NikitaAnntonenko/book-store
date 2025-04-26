e5854aa5264618ed92367b60f89d423a59c27333
nikitaantonenko <nikita.anntonenko@gmail.com>
2025-04-26 22:37:25 +0200

    Fix UI API

--- bookstore-ui/src/services/api.js
+++ bookstore-ui/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getCurrentToken } from './authService';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8081/api',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8081/api',
 });
 
 api.interceptors.request.use(
